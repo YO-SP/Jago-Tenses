e706dad6b9a6a764a67bd724f9946b77d521a753
Yakobus Octavianus Sucahyo Putra <yakobusavin2004@gmail.com>
2026-01-07 13:52:51 +0700

    Mengubah tampilan navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,16 @@ export default function Header(){
 
     return(
         <>  
-            <div class="flex bg-[#000000] p-4 justify-between w-full h-full">
-                <Link to="/"><div class="text-white text-2xl font-medium px-3 py-2">JAGOTENSES</div></Link>
-                <nav class="flex gap-4 text-white text-xl font-medium">
-                    <Link to="/lessons"><div class="px-3 py-2 hover:bg-white hover:text-black rounded-full duration-100">Lessons</div></Link>
-                    <Link to="/about"><div class="px-3 py-2 hover:bg-white hover:text-black rounded-full duration-100">About me</div></Link>
-                    <Link to="/contact"><div class="px-3 py-2 hover:bg-white hover:text-black rounded-full duration-100">Contact</div></Link>
+            <div class="flex bg-[#ffffff] p-4 justify-between w-full h-full">
+                <Link to="/"><div class="text-black text-2xl font-medium px-3 py-2">JAGOTENSES</div></Link>
+                <nav class="flex gap-4 text-black text-xl font-medium">
+                    <Link to="/lessons"><div class="px-3 py-2 hover:bg-black hover:text-white rounded-full duration-100">Lessons</div></Link>
+                    <Link to="/about"><div class="px-3 py-2 hover:bg-black hover:text-white rounded-full duration-100">About me</div></Link>
+                    <Link to="/contact"><div class="px-3 py-2 hover:bg-black hover:text-white rounded-full duration-100">Contact</div></Link>
                 </nav>
             </div>
+
+            <hr class="border-2 border-gray-300 "></hr>
         </>
     )
 }
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -5,7 +5,22 @@ export default function AboutMe(){
     return(
         <>
           <Header></Header>
-            <h1>ini halaman about me</h1>
+          
+          <h1 class="text-black font-['Archivo_Black'] text-center text-5xl mt-15">About Me</h1>
+
+          <div class="flex justify-center gap-15 mt-20 ml-55">
+            <img class="w-50 h-50 mr-5" src="src/assets/Frame 4.svg" />
+            
+            <div class="flex flex-col">
+              <p class="mr-80 text-2xl w-150 text-justify">I am an Informatics Engineering student and a relentless problem-solver on a mission to become a high-impact Fullstack Developer. Currently, I’m deep-diving into the MERN Stack to build seamless digital experiences, while simultaneously venturing into the future of the web through Blockchain and Solidity.</p>
+              <br></br>
+              <p class="mr-80 text-2xl w-150 text-justify">Driven by curiosity and a global mindset, I’m constantly refining my craft and my English proficiency to bridge the gap between complex code and real-world solutions. I don't just write code; I build the tools of tomorrow."</p>
+            </div>
+          </div>
+            
+    
+
+          
           <Footer></Footer>
         </>
     )
