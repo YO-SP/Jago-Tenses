92369843ef00f41b3a7d4cb7ed86e0714809da0c
Yakobus Octavianus Sucahyo Putra <yakobusavin2004@gmail.com>
2026-01-07 16:33:01 +0700

    Membuat card masing-masing tenses

---
+++ src/data/tenses.js
@@ -0,0 +1,39 @@
+export const tenses = [
+    {
+        id: "present-simple", 
+        title: "Present Simple", 
+        description: "Aku suka kamu"
+    },
+
+    {
+        id: "present-continuous", 
+        title: "Present Continuous"
+    },
+
+    {
+        id: "past-simple", 
+        title: "Past Simple"
+    },
+
+    {
+        id: "present-perfect", 
+        title: "Present Perfect"
+    },
+
+    {
+        id: "present-perfect-continuous", 
+        title: "Present Perfect Continuous"
+    },
+
+    {
+        id: "past-perfect-continuous", 
+        title: "Past Perfect Continuous"
+    },
+
+    {
+        id: "past-continuous", 
+        title: "Past Continuous"
+    },
+]
+
+
--- src/pages/Lessons.jsx
+++ src/pages/Lessons.jsx
@@ -1,23 +1,30 @@
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
+import { Link } from 'react-router-dom'; 
+import { tenses } from '../data/tenses.js'; 
 
 export default function Lessons(){
     return(
         <>
         <Header></Header>
 
+        <h1 class="text-black font-['Archivo_Black'] text-center text-5xl mt-15 mb-20">What do you want to learn today?</h1>        
 
+        <div className="flex justify-center mt-12">
+        <div className="grid grid-cols-4 gap-6">
+          {tenses.map((tense) => (
+            <Link key={tense.id} to={`/lessons/${tense.id}`}>
+              <div className="border border-black rounded-xl p-6 hover:bg-amber-200 hover:text-black duration-200">
+                <h2 className="font-['Archivo_Black'] text-xl">
+                  {tense.title}
+                </h2>
+                <p className="mt-2">{tense.description}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+        </div>
 
-
-        <h1>ini halaman Lessons</h1>
-
-
-
-
-
-
-
-        
         <Footer></Footer>
         </>
     )
